f74e748c457b2f390c3bcc73e4df9114b6e8efa8
Ron Panduwana <panduwana@gmail.com>
2015-01-19 19:16:16 +0700

    permitall for api like /api/oc/*

--- src/main/java/com/supadosworks/jhipster/oc/config/SecurityConfiguration.java
+++ src/main/java/com/supadosworks/jhipster/oc/config/SecurityConfiguration.java
@@ -103,6 +103,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/api/register").permitAll()
                 .antMatchers("/api/activate").permitAll()
                 .antMatchers("/api/authenticate").permitAll()
+                .antMatchers("/api/oc/**").permitAll()
                 .antMatchers("/api/logs/**").hasAuthority(AuthoritiesConstants.ADMIN)
                 .antMatchers("/api/**").authenticated()
                 .antMatchers("/metrics/**").hasAuthority(AuthoritiesConstants.ADMIN)
